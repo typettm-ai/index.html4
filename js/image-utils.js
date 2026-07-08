ef84d36c0867bd0bf523b7551c5584e1ea4fcb1f
typettm-ai <typettm@icloud.com>
2026-07-08 21:00:52 +0900

    CarQR β v0.2.2 image-utils追加

---
+++ js/image-utils.js
@@ -0,0 +1,16 @@
+// CarQR β v0.2.2
+// image-utils.js
+
+function createCanvasFromImage(image) {
+
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = image.naturalWidth;
+    canvas.height = image.naturalHeight;
+
+    ctx.drawImage(image, 0, 0);
+
+    return canvas;
+
+}
